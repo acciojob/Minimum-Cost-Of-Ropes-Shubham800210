3c736972cec3ce667f6119f537fb2df9d553590d
shubham kumar <65395298+Shubham800210@users.noreply.github.com>
2023-03-07 20:59:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 //your code here
-function minCostToFormRope(a) {
-  a.preventDefault();
-  var inputElement = document.querySelector("input").value;
-  var arr = inputElement.split(",");
-  arr.sort(function (a, b) {
-    return a - b;
-  });
-
-  // we need to access first two element
-  // add them & store in a variable res
-
-  // add the res in array
-  // increment cost by res
-  var cost = 0;
-  while (arr.length > 1) {
-    var res = Number(arr[0]) + Number(arr[1]);
-    arr.splice(0, 2);
-
-    arr.push(res);
-    cost += res;
-
-    arr.sort(function (a, b) {
-      return a - b;
-    });
-  }
-
-  document.getElementById("result").textContent = cost;
+function minCost(event) {
+	event.preventDefault();
+	var input = document.querySelector("input").value;
+	var arr = input.split(",");
+	var cost=0;
+	while(arr.length > 1){
+		arr.sort(function (a,b) {return a-b});
+		var temp=Number(arr[0]) + Number(arr[1]);
+		cost+=temp;
+		arr.push(temp);
+		arr.splice(0,2);
+		arr.sort(function (a,b) {return a-b});
+		
+	}
+	document.querySelector("div").textContent=cost;
 }
